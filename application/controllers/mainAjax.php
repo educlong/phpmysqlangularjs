349b1eb0afd81183f2ef806eac5d3fcce9dea40a
educlong <educlong@gmail.com>
2021-07-28 01:14:24 -0400

    Update3: Xử lý phần add thêm thông tin mới bằng ajax (phần upload lên db và update chưa làm đc)

--- application/controllers/mainAjax.php
+++ application/controllers/mainAjax.php
@@ -46,29 +46,6 @@ class mainAjax extends CI_Controller {	/*Link truy cập để text: http://127.
 		$this->load->view('mainViewAjax', array("updateEmployeeDetail"=> $this->mainModel->checkEmployeeModel($idEmployee)),FALSE);
 	}
 
-	public function updateEmployee()
-	{
-		$imgAvatarEmp = $this->uploadImageAvatar();
-
-		if ($imgAvatarEmp)  /*xử lý hình ảnh avatar, nếu có upload hình ảnh mới*/
-			$imgAvatarEmp = base_url()."files/".$this->uploadImageAvatar();
-		else 				/*nếu k có thì zữ nguyên*/
-			$imgAvatarEmp = $this->input->post('avatarEmployeeNotChange');
-		$this->load->model('mainModel');
-		if ($this->mainModel->updateEmployeeModel($this->input->post('idEmployee'),
-											  $this->input->post('nameEmployee'),
-											  $this->input->post('ageEmployee'),
-											  $this->input->post('phoneEmployee'),
-											  $imgAvatarEmp,
-											  $this->input->post('ordersEmployee'),
-											  $this->input->post('linkfbEmployee')))
-		{
-			$this->load->view('insertupdatedeleteView');
-		}
-		else echo 'update fail';
-
-	}
-
 	public function deleteEmployee($idEmployee)
 	{
 		$this->load->model('mainModel');
@@ -79,61 +56,6 @@ class mainAjax extends CI_Controller {	/*Link truy cập để text: http://127.
 	}
 
 
-
-
-	public function uploadImageAvatar()
-	{
-		/*phần xử lý upload file*/
-		$target_dir = "files/";	/*đường dẫn file*/
-		$target_file = $target_dir . basename($_FILES["avatarEmployee"]["name"]);
-		$uploadOk = 1;
-		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-		  $check = getimagesize($_FILES["avatarEmployee"]["tmp_name"]);
-		  if($check !== false) {
-		    // echo "File is an image - " . $check["mime"] . ".";
-		    $uploadOk = 1;
-		  } else {
-		    // echo "File is not an image.";
-		    $uploadOk = 0;
-		  }
-		}
-
-		// Check if file already exists
-		if (file_exists($target_file)) {
-		  // echo "Sorry, file already exists.";
-		  $uploadOk = 0;
-		}
-
-		// Check file size
-		if ($_FILES["avatarEmployee"]["size"] > 5000000) {
-		  // echo "Sorry, your file is too large.";
-		  $uploadOk = 0;
-		}
-
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-		  // echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		  $uploadOk = 0;
-		}
-
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-		  // echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} 
-		else {
-		  if (move_uploaded_file($_FILES["avatarEmployee"]["tmp_name"], $target_file)) {
-		    // echo "The file ". htmlspecialchars( basename( $_FILES["avatarEmployee"]["name"])). " has been uploaded.";
-		  } else {
-		    // echo "Sorry, there was an error uploading your file.";
-		  }
-		}
-		return basename($_FILES["avatarEmployee"]["name"]);
-	}
 
 
 	/*xử lý phần upload hình ảnh bằng ajax*/
