fc68f21c72a911a0f0ee7b5f20156282f481af0c
educlong <educlong@gmail.com>
2021-07-29 18:44:56 -0400

    angularjs basic (Các khái niệm ban đầu: ng-app, ng-controller, ng-model/init/show/hide/click/repeat, ng-view)

--- application/views/AttributeHeaderView.php
+++
@@ -1,8 +0,0 @@
-<ol class="breadcrumb">
-	<li class="breadcrumb-item"><a href="<?php echo base_url() ?>AttributeHomejson">Home</a></li>
-	<li class="breadcrumb-item"><a href="<?php echo base_url() ?>AttributeHomejson/insertAttributeSlideOpenning">Insert Data</a></li>
-	<li class="breadcrumb-item"><a href="<?php echo base_url() ?>AttributeHomejson/updateAttributeSlideOpenning">Update data</a></li>
-	<li class="breadcrumb-item"><a href="<?php echo base_url() ?>AttributeHomejson/insertAttributeMenuOpenning">Quản lý menu</a></li>
-	<li class="breadcrumb-item"><a href="<?php echo base_url() ?>AttributeHomejson/createAccount">Create an account</a></li>
-	<li class="breadcrumb-item active"></li>
-</ol>
